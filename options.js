21db06b9173f8d2c0d0474543d57d885884c8fa6
Albert <wuyichang1990@gmail.com>
2020-08-19 17:35:00 +0100

    finally

--- options.js
+++ options.js
@@ -11,7 +11,7 @@ save.onclick = function(element){
   chrome.storage.sync.set({'sites': sites.value.trim().split('\n')}, function(){
     var saveNote = {
       type:'basic',
-      iconUrl:'images\\get_started48.png',
+      iconUrl:'images\\favicon48.png',
       title:'Sites saved',
       message:"your sites are successfully saved. Great."
     };
@@ -25,32 +25,10 @@ reset.onclick = function(element){
   sites.value = "";
   var resetNote = {
     type:'basic',
-    iconUrl:'images\\get_started48.png',
+    iconUrl:'images\\favicon48.png',
     title:'Sites reset',
     message:"your sites are successfully reseted. Great."
   };
   chrome.notifications.create('resetNote',resetNote);
 }
 
-
-
-
-
-
-
-
-// let page = document.getElementById('buttonDiv');
-// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-// function constructOptions(kButtonColors) {
-//   for (let item of kButtonColors) {
-//     let button = document.createElement('button');
-//     button.style.backgroundColor = item;
-//     button.addEventListener('click', function() {
-//       chrome.storage.sync.set({color: item}, function() {
-//         console.log('color is ' + item);
-//       })
-//     });
-//     page.appendChild(button);
-//   }
-// }
-// constructOptions(kButtonColors);
